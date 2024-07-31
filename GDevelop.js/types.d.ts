9a5211e75a6647c04acc24434d23cfc0d85bfa70
Davy Hélard <davy.helard@gmail.com>
2024-08-01 01:08:14 +0200

    Fix some GDevelop.js tests.

--- GDevelop.js/types.d.ts
+++ GDevelop.js/types.d.ts
@@ -2064,6 +2064,7 @@ export class ParameterMetadataContainer extends EmscriptenObject {
   getParametersCount(): number;
   getParameterPosition(parameterMetadata: ParameterMetadata): number;
   clearParameters(): void;
+  addNewParameter(name: string): ParameterMetadata;
 }
 
 export class EventsFunctionsContainer extends EmscriptenObject {
